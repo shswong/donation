0d5d3fe2992780689a86ad953267a27a6553a557
Stephen Wong <valvalisofthewind@gmail.com>
2014-04-17 21:20:06 -0700

    setting up functions

--- src/ButtonMenu.java
+++ src/ButtonMenu.java
@@ -1,5 +1,3 @@
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,8 +5,10 @@ import java.awt.event.ActionListener;
 
 /*
  *	ButtonMenu creates JButtons that will each do their own purpose.
+ *
  */
-public class ButtonMenu extends JPanel implements ActionListener {
+
+public class ButtonMenu extends JPanel {
 	private int buttonCount;
 
 	public ButtonMenu() {
@@ -17,48 +17,42 @@ public class ButtonMenu extends JPanel implements ActionListener {
 
 		JButton memberMaintenance = new JButton(new AbstractAction("Member Maintenance") {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Member Maintenance");
-				new JFrame("Member Maintenance").setVisible(true);
+				MemberMaintenance manager = new MemberMaintenance();
 			}
 		});
 		printButton(memberMaintenance);
 
 		JButton donationMaintenance = new JButton(new AbstractAction("Donation Type Maintenance") {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Donation Type Maintenance");
-				new JFrame("Donation Type Maintenance").setVisible(true);
+				// new window to open donation maintenance
 			}
 		});
 		printButton(donationMaintenance);
 
 		JButton enterDonation = new JButton(new AbstractAction("Enter Donation") {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Enter Donation");
-				new JFrame("Enter Donation").setVisible(true);
+				// new window to enter donations
 			}
 		});
 		printButton(enterDonation);
 
 		JButton weeklyCheck = new JButton(new AbstractAction("Run Weekly Check Offering Report") {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Run Weekly Check Offering Report");
-				new JFrame("Run Weekly Check Offering Report").setVisible(true);
+				// new window to run a weekly check offering report
 			}
 		});
 		printButton(weeklyCheck);
 
 		JButton weeklyCash = new JButton(new AbstractAction("Run Weekly Cash Offering Report") {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Run Weekly Cash Offering Report");
-				new JFrame("Run Weekly Cash Offering Report").setVisible(true);
+				// new window to run weekly cash offering report
 			}
 		});
 		printButton(weeklyCash);
 
 		JButton donationByMember = new JButton(new AbstractAction("Run Donation By Member by Date Range Report") {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(null, "Run Donation By Member by Date Range Report");
-				new JFrame("Run Donation By Member by Date Range Report").setVisible(true);
+				// new window to run member donation by date report
 			}
 		});
 		printButton(donationByMember);
@@ -68,9 +62,5 @@ public class ButtonMenu extends JPanel implements ActionListener {
 		buttonCount += 1;
 		button.setBounds(624, 8 + (buttonCount * 50), 350, 40);
 		add(button);
-	}
-
-	public void actionPerformed(ActionEvent e) {
-
 	}
 }
--- src/MemberMaintenance.java
+++ src/MemberMaintenance.java
@@ -1,4 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MemberMaintenance {
+/*
+ * MemberMaintenance will define member management options and functions
+ *
+ */
 
+public class MemberMaintenance extends JPanel {
+	private int buttonCount;
+
+	public MemberMaintenance() {
+		buttonCount = 0;
+		createAndShowGUI();
+
+		JButton addMember = new JButton(new AbstractAction("Add a member") {
+			public void actionPerformed(ActionEvent e) {
+				// new window to add member
+			}
+		});
+		printButton(addMember);
+
+		JButton deleteMember = new JButton(new AbstractAction("Delete a member") {
+			public void actionPerformed(ActionEvent e) {
+				// new window to delete member
+			}
+		});
+		printButton(deleteMember);
+	}
+
+	private void createAndShowGUI() {
+		JFrame frame = new JFrame("Member Maintenance");
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		JMenuBar menu = new JMenuBar();
+
+		frame.setPreferredSize(new Dimension(800, 600));
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	public void printButton(JButton button) {
+		buttonCount += 1;
+		button.setBounds(424, 8 + (buttonCount * 50), 350, 40);
+		add(button);
+	}
 }
