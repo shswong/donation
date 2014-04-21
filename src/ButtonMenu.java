5bb6852a2cb601123cd3a499ed7b3834b0dcc0cd
Stephen Wong <valvalisofthewind@gmail.com>
2014-04-20 23:49:28 -0700

    getting ready to implement member functions

--- src/ButtonMenu.java
+++ src/ButtonMenu.java
@@ -17,50 +17,65 @@ public class ButtonMenu extends JPanel {
 
 		JButton memberMaintenance = new JButton(new AbstractAction("Member Maintenance") {
 			public void actionPerformed(ActionEvent e) {
-				MemberMaintenance manager = new MemberMaintenance();
+				createAndShowGUI("Member Maintenance", new MemberMaintenance());
 			}
 		});
-		printButton(memberMaintenance);
 
 		JButton donationMaintenance = new JButton(new AbstractAction("Donation Type Maintenance") {
 			public void actionPerformed(ActionEvent e) {
 				// new window to open donation maintenance
 			}
 		});
-		printButton(donationMaintenance);
 
 		JButton enterDonation = new JButton(new AbstractAction("Enter Donation") {
 			public void actionPerformed(ActionEvent e) {
 				// new window to enter donations
 			}
 		});
-		printButton(enterDonation);
 
 		JButton weeklyCheck = new JButton(new AbstractAction("Run Weekly Check Offering Report") {
 			public void actionPerformed(ActionEvent e) {
 				// new window to run a weekly check offering report
 			}
 		});
-		printButton(weeklyCheck);
 
 		JButton weeklyCash = new JButton(new AbstractAction("Run Weekly Cash Offering Report") {
 			public void actionPerformed(ActionEvent e) {
 				// new window to run weekly cash offering report
 			}
 		});
-		printButton(weeklyCash);
 
 		JButton donationByMember = new JButton(new AbstractAction("Run Donation By Member by Date Range Report") {
 			public void actionPerformed(ActionEvent e) {
 				// new window to run member donation by date report
 			}
 		});
+
+		printButton(memberMaintenance);
+		printButton(donationMaintenance);
+		printButton(enterDonation);
+		printButton(weeklyCheck);
+		printButton(weeklyCash);
 		printButton(donationByMember);
 	}
 
-	public void printButton(JButton button) {
+	private void printButton(JButton button) {
 		buttonCount += 1;
 		button.setBounds(624, 8 + (buttonCount * 50), 350, 40);
 		add(button);
+	}
+
+	private static void createAndShowGUI(String header, JComponent component) {
+		JFrame frame = new JFrame(header);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		JMenuBar menu = new JMenuBar();
+		component.setOpaque(true);
+		frame.setContentPane(component);
+
+		frame.setPreferredSize(new Dimension(800, 600));
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 }
--- src/MemberMaintenance.java
+++ src/MemberMaintenance.java
@@ -10,41 +10,102 @@ import java.awt.event.ActionListener;
 
 public class MemberMaintenance extends JPanel {
 	private int buttonCount;
+	private int labelCount;
+	private int textFieldCount;
 
 	public MemberMaintenance() {
 		buttonCount = 0;
-		createAndShowGUI();
+		labelCount = 0;
+		textFieldCount = 0;
+		setLayout(null);
+
+		JLabel labelLastName = new JLabel("Last Name:");
+		JLabel labelFirstName = new JLabel("First Name:");
+		JLabel labelAddress = new JLabel("Address:");
+		JLabel labelCity = new JLabel("City:");
+		JLabel labelState = new JLabel("State:");
+		JLabel labelZip = new JLabel("Zip:");
+		JLabel labelHome = new JLabel("Home Phone:");
+		JLabel labelCell = new JLabel("Cell Phone:");
+		JLabel labelFax = new JLabel("Fax:");
+
+		JFormattedTextField textLastName = new JFormattedTextField();
+		JFormattedTextField textFirstName = new JFormattedTextField();
+		JFormattedTextField textAddress = new JFormattedTextField();
+		JFormattedTextField textCity = new JFormattedTextField();
+		JFormattedTextField textState = new JFormattedTextField();
+		JFormattedTextField textZip = new JFormattedTextField();
+		JFormattedTextField textHome = new JFormattedTextField();
+		JFormattedTextField textCell = new JFormattedTextField();
+		JFormattedTextField textFax = new JFormattedTextField();
+
+		JButton test = new JButton(new AbstractAction("Test") {
+			public void actionPerformed(ActionEvent e) {
+				// button to test input functions
+			}
+		});
+
+		JButton saveMember = new JButton(new AbstractAction("Save Changes") {
+			public void actionPerformed(ActionEvent e) {
+				// save changes function
+			}
+		});
 
 		JButton addMember = new JButton(new AbstractAction("Add a member") {
 			public void actionPerformed(ActionEvent e) {
-				// new window to add member
+				// toggle frame to add member functions
 			}
 		});
-		printButton(addMember);
 
 		JButton deleteMember = new JButton(new AbstractAction("Delete a member") {
 			public void actionPerformed(ActionEvent e) {
-				// new window to delete member
+				// toggle frame to delete member functions
 			}
 		});
+
+		printLabel(labelLastName);
+		printLabel(labelLastName);
+		printLabel(labelFirstName);
+		printLabel(labelAddress);
+		printLabel(labelCity);
+		printLabel(labelState);
+		printLabel(labelZip);
+		printLabel(labelHome);
+		printLabel(labelCell);
+		printLabel(labelFax);
+
+		printTextField(textLastName);
+		printTextField(textLastName);
+		printTextField(textFirstName);
+		printTextField(textAddress);
+		printTextField(textCity);
+		printTextField(textState);
+		printTextField(textZip);
+		printTextField(textHome);
+		printTextField(textCell);
+		printTextField(textFax);
+
+		printButton(test);
+		printButton(saveMember);
+		printButton(addMember);
 		printButton(deleteMember);
 	}
 
-	private void createAndShowGUI() {
-		JFrame frame = new JFrame("Member Maintenance");
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-		JMenuBar menu = new JMenuBar();
-
-		frame.setPreferredSize(new Dimension(800, 600));
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+	private void printButton(JButton button) {
+		buttonCount += 1;
+		button.setBounds(624, 8 + (buttonCount * 50), 150, 40);
+		add(button);
 	}
 
-	public void printButton(JButton button) {
-		buttonCount += 1;
-		button.setBounds(424, 8 + (buttonCount * 50), 350, 40);
-		add(button);
+	private void printLabel(JLabel label) {
+		labelCount += 1;
+		label.setBounds(24, 8 + (labelCount * 30), 100, 20);
+		add(label);
+	}
+
+	private void printTextField(JFormattedTextField textField) {
+		textFieldCount += 1;
+		textField.setBounds(104, 8 + (textFieldCount * 30), 150, 20);
+		add(textField);
 	}
 }
